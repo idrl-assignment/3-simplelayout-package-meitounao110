7c031aba52284e637d8c2ee9f55c14ab9beaa24a
zyy <431041317@qq.com>
2020-11-30 22:43:09 +0800

    test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="A simplelayout package",
     long_description_content_type="text/markdown",
     url="https://github.com/idrl-assignment/3-simplelayout-package-meitounao110",
-    packages=['src'],
+    packages=find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
