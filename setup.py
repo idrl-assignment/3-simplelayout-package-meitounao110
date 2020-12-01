50523da88c966d9d578eb05f7eec56729dbd4eed
zyy <431041317@qq.com>
2020-12-01 09:10:00 +0800

    test

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 import setuptools
 
-
 setuptools.setup(
     name="simplelayout-meitounao110",  # Replace with your own username
     version="0.0.1",
     author="meitounao110",
     author_email="none",
     description="A simplelayout package",
-    long_description_content_type="text/markdown",
     url="https://github.com/idrl-assignment/3-simplelayout-package-meitounao110",
-    packages=setuptools.find_packages('src'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: linux",
-    ],
-    python_requires='>=3.6',
+    # package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
     install_requires=['matplotlib', 'numpy', 'scipy', 'pytest'],
     entry_points={
         'console_scripts': [  # 配置生成命令行工具及入口
